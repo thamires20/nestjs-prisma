357dd2a5ae427ec142af7c1218725123019fd2d6
thamires20 <96100998+thamires20@users.noreply.github.com>
2023-12-27 22:56:52 -0300

    alteraçao rota get all

--- nestjs-prisma/src/book/book.controller.ts
+++ nestjs-prisma/src/book/book.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Res} from "@nestjs/common";
 import { Book } from "./book.model";
 import { BookService } from "./book.service";
+import { Request, Response } from "express";
 
 @Controller('api/v1/book')
 export class Bookcontroller{
   constructor(private readonly bookService: BookService){}
 
   @Get()
-  async getAllBook():Promise<Book[]>{
-    return this.bookService.getAllBook()
-  }
+   async getAllBook(@Req() request:Request, @Res() response:Response):Promise<any>{
+    const result = await this.bookService.getAllBook()
+    return response.status(200).json({
+      status: "Ok!",
+      message:"Successfully fetch data!",
+      result: result
+    })
+   }
+
 @Post()
   async postBook(@Body() postData: Book): Promise<Book>{
     return this.bookService.createBook(postData)
@@ -28,3 +35,12 @@ export class Bookcontroller{
     return this.bookService.updateBook(id, postData)
   }
 }
+
+
+
+
+// ROTA SIMPLES GET ALL PRA BOOK
+  // @Get()
+  // async getAllBook():Promise<Book[]>{
+  //   return this.bookService.getAllBook()
+  // }
